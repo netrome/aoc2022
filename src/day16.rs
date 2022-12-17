e59024eeda5b20df0cb8671cd788963ff6c2edce
Mårten Blankfors <marten@blankfors.se>
2022-12-17 01:38:42 +0100

    Day 16 solved! Phew

--- src/day16.rs
+++ src/day16.rs
@@ -1,18 +1,43 @@
 pub fn p1(input: &str) -> String {
     let graph: Graph = input.parse().unwrap();
     let graph2: Graph2 = graph.into();
+    let subgraph = graph2.valves.keys().cloned().collect();
 
-    find_max_pressure(&graph2).to_string()
+    find_max_pressure(&graph2, subgraph, 30).to_string()
 }
 
 pub fn p2(input: &str) -> String {
-    todo!();
+    let graph: Graph = input.parse().unwrap();
+    let graph2: Graph2 = graph.into();
+
+    find_max_pressure_with_elephant(&graph2).to_string()
 }
 
-fn find_max_pressure(graph: &Graph2) -> u32 {
-    let all_nodes = graph.valves.keys().cloned().collect();
+fn find_max_pressure_with_elephant(graph: &Graph2) -> u32 {
+    let all_nodes: HashSet<_> = graph.valves.keys().cloned().collect();
 
-    let mut high_score = Rc::new(PathNode::genesis(all_nodes));
+    let mut max_pressure = 0;
+
+    let mut i = 0;
+
+    for elephant_share in all_nodes.iter().powerset() {
+        let elephant_set: HashSet<_> = elephant_share.into_iter().cloned().collect();
+        let santa_set: HashSet<_> = all_nodes.difference(&elephant_set).cloned().collect();
+
+        let elephant_pressure = find_max_pressure(graph, elephant_set, 26);
+        let santa_pressure = find_max_pressure(graph, santa_set, 26);
+
+        let pressure = elephant_pressure + santa_pressure;
+        if pressure > max_pressure {
+            max_pressure = pressure
+        }
+    }
+
+    max_pressure
+}
+
+fn find_max_pressure(graph: &Graph2, subgraph: HashSet<ValveId>, max_time: u32) -> u32 {
+    let mut high_score = Rc::new(PathNode::genesis(subgraph));
     let mut nodes_to_visit = Vec::new();
     nodes_to_visit.push(high_score.clone());
 
@@ -21,7 +46,7 @@ fn find_max_pressure(graph: &Graph2) -> u32 {
             high_score = node.clone();
         }
 
-        nodes_to_visit.append(&mut PathNode::children(node, graph));
+        nodes_to_visit.append(&mut PathNode::children(node, graph, max_time));
     }
 
     high_score.score
@@ -47,14 +72,14 @@ impl PathNode {
         }
     }
 
-    fn children(node: Rc<Self>, graph: &Graph2) -> Vec<Rc<Self>> {
+    fn children(node: Rc<Self>, graph: &Graph2, max_time: u32) -> Vec<Rc<Self>> {
         let mut children = Vec::new();
 
         for valve_id in node.to_visit.iter() {
             let next = graph.valves.get(&valve_id).unwrap();
             let dist = next.tunnels.get(&node.pos).unwrap();
 
-            let remaining_time = 30u32.saturating_sub(node.minute + dist + 1);
+            let remaining_time = max_time.saturating_sub(node.minute + dist + 1);
             let mut to_visit = node.to_visit.clone();
             to_visit.remove(&valve_id);
 
@@ -245,5 +270,6 @@ use std::{
 };
 
 use crate::solution::Solution;
+use itertools::Itertools;
 inventory::submit!(Solution::new(16, 1, p1));
 inventory::submit!(Solution::new(16, 2, p2));
