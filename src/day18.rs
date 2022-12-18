7212cb6dbc97950110a5dc14c927281d06e26d14
Mårten Blankfors <marten@blankfors.se>
2022-12-18 20:28:36 +0100

    Day 18 part 1 solved

--- src/day18.rs
+++ src/day18.rs
@@ -1,43 +1,80 @@
 pub fn p1(input: &str) -> String {
-    calorie_sums(input).max().unwrap().to_string()
+    let grid: Grid = input.parse().expect("Nooo");
+    grid.surface_area().to_string()
 }
 
 pub fn p2(input: &str) -> String {
-    k_largest(3, calorie_sums(input))
-        .into_iter()
-        .sum::<usize>()
-        .to_string()
+    todo!();
 }
 
-fn calorie_sums(input: &str) -> impl Iterator<Item = usize> + '_ {
-    input.split("\n\n").map(|row| {
-        row.split_whitespace()
-            .map(|item| item.parse::<usize>().unwrap())
-            .sum::<usize>()
-    })
+#[derive(Debug)]
+struct Grid {
+    cubes: HashSet<Cube>,
 }
 
-pub fn k_largest(k: usize, iter: impl Iterator<Item = usize>) -> Vec<usize> {
-    let mut iter = iter.map(Reverse);
-
-    let mut heap = BinaryHeap::new();
-
-    for _ in 0..k {
-        heap.push(iter.next().unwrap());
+impl Grid {
+    fn surface_area(&self) -> usize {
+        self.cubes.iter().map(|cube| cube.surface_area(self)).sum()
     }
-
-    iter.for_each(|item| {
-        if item.0 > heap.peek().unwrap().0 {
-            heap.pop();
-            heap.push(item)
-        }
-    });
-
-    heap.into_iter().map(|Reverse(item)| item).collect()
 }
 
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
+impl FromStr for Grid {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let cubes = s
+            .lines()
+            .map(|line| line.parse())
+            .collect::<Result<_, _>>()?;
+
+        Ok(Self { cubes })
+    }
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+struct Cube {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+impl Cube {
+    fn surface_area(&self, grid: &Grid) -> usize {
+        let (mut left, mut right, mut in_, mut out, mut up, mut down) = (
+            self.clone(),
+            self.clone(),
+            self.clone(),
+            self.clone(),
+            self.clone(),
+            self.clone(),
+        );
+
+        left.x += 1;
+        right.x -= 1;
+        in_.y += 1;
+        out.y -= 1;
+        up.z += 1;
+        down.z -= 1;
+
+        [left, right, in_, out, up, down]
+            .into_iter()
+            .filter(|cube| !grid.cubes.contains(&cube))
+            .count()
+    }
+}
+
+impl FromStr for Cube {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (x, y, z) = sscanf::sscanf!(s.trim(), "{i64},{i64},{i64}")
+            .map_err(|e| anyhow::anyhow!("Noo {}", e))?;
+
+        Ok(Self { x, y, z })
+    }
+}
+
+use std::{collections::HashSet, str::FromStr};
 
 use crate::solution::Solution;
 inventory::submit!(Solution::new(18, 1, p1));
