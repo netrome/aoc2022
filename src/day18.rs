ed41340cd2eb53fbd281431d7c24938ae40d4553
Mårten Blankfors <marten@blankfors.se>
2022-12-18 20:49:45 +0100

    Day 18 solved

--- src/day18.rs
+++ src/day18.rs
@@ -4,7 +4,42 @@ pub fn p1(input: &str) -> String {
 }
 
 pub fn p2(input: &str) -> String {
-    todo!();
+    let grid: Grid = input.parse().expect("Nooo");
+    let steam = compute_steam(&grid);
+    grid.surface_area_2(&steam).to_string()
+}
+
+fn compute_steam(grid: &Grid) -> HashSet<Cube> {
+    let max_x = grid.cubes.iter().map(|c| c.x).max().unwrap();
+    let max_y = grid.cubes.iter().map(|c| c.y).max().unwrap();
+    let max_z = grid.cubes.iter().map(|c| c.z).max().unwrap();
+
+    let mut to_expand = vec![Cube::origin()];
+    let mut res = HashSet::new();
+
+    while let Some(steam) = to_expand.pop() {
+        for neighbor in steam.neighbors() {
+            if neighbor.x > max_x + 1 || neighbor.x < -1 {
+                continue;
+            }
+
+            if neighbor.y > max_y + 1 || neighbor.y < -1 {
+                continue;
+            }
+
+            if neighbor.z > max_z + 1 || neighbor.z < -1 {
+                continue;
+            }
+
+            if !grid.cubes.contains(&neighbor) && !res.contains(&neighbor) {
+                to_expand.push(neighbor)
+            }
+        }
+
+        res.insert(steam);
+    }
+
+    res
 }
 
 #[derive(Debug)]
@@ -15,6 +50,13 @@ struct Grid {
 impl Grid {
     fn surface_area(&self) -> usize {
         self.cubes.iter().map(|cube| cube.surface_area(self)).sum()
+    }
+
+    fn surface_area_2(&self, steam: &HashSet<Cube>) -> usize {
+        self.cubes
+            .iter()
+            .map(|cube| cube.surface_area_2(&steam))
+            .sum()
     }
 }
 
@@ -39,7 +81,25 @@ struct Cube {
 }
 
 impl Cube {
+    fn origin() -> Self {
+        Self { x: 0, y: 0, z: 0 }
+    }
+
     fn surface_area(&self, grid: &Grid) -> usize {
+        self.neighbors()
+            .into_iter()
+            .filter(|cube| !grid.cubes.contains(&cube))
+            .count()
+    }
+
+    fn surface_area_2(&self, steam: &HashSet<Cube>) -> usize {
+        self.neighbors()
+            .into_iter()
+            .filter(|cube| steam.contains(&cube))
+            .count()
+    }
+
+    fn neighbors(&self) -> [Cube; 6] {
         let (mut left, mut right, mut in_, mut out, mut up, mut down) = (
             self.clone(),
             self.clone(),
@@ -57,9 +117,6 @@ impl Cube {
         down.z -= 1;
 
         [left, right, in_, out, up, down]
-            .into_iter()
-            .filter(|cube| !grid.cubes.contains(&cube))
-            .count()
     }
 }
 
