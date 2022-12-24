07d9a542b72217f78b7ff83a01e6ae2d6ef05335
Mårten Blankfors <marten@blankfors.se>
2022-12-24 15:21:16 +0100

    Day 22 boilerplate ready

---
+++ src/day22.rs
@@ -0,0 +1,11 @@
+pub fn p1(input: &str) -> String {
+    todo!();
+}
+
+pub fn p2(input: &str) -> String {
+    todo!();
+}
+
+use crate::solution::Solution;
+inventory::submit!(Solution::new(22, 1, p1));
+inventory::submit!(Solution::new(22, 2, p2));
