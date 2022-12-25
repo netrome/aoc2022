f1dc38727446e22718b94ce2e13389a5c5f1f4d5
Mårten Blankfors <marten@blankfors.se>
2022-12-25 23:16:11 +0100

    WIP...

--- src/day24.rs
+++ src/day24.rs
@@ -6,6 +6,7 @@ pub fn p2(input: &str) -> String {
     todo!();
 }
 
+#[derive(Debug)]
 struct World {
     minute: usize,
     santa_possible_points: HashSet<Pos>,
@@ -15,6 +16,7 @@ struct World {
     max_y: i64,
 }
 
+#[derive(Debug)]
 struct Blizzard {
     pos: Pos,
     direction: Direction,
@@ -44,7 +46,7 @@ impl Blizzard {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Pos(i64, i64);
 
 impl Pos {
@@ -70,7 +72,53 @@ enum Direction {
     Down,
 }
 
-use std::collections::{HashMap, HashSet};
+impl FromStr for World {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut blizzards = Vec::new();
+        let mut santa_possible_points = HashSet::new();
+        let max_x = (s.lines().count() - 1) as i64;
+        let max_y = (s.lines().next().unwrap().len() - 1) as i64;
+
+        for (x, line) in s.trim().lines().enumerate() {
+            for (y, c) in line.chars().enumerate() {
+                let direction = match c {
+                    '^' => Direction::Up,
+                    '>' => Direction::Right,
+                    '<' => Direction::Left,
+                    'v' => Direction::Down,
+                    _ => continue,
+                };
+
+                let blizzard = Blizzard {
+                    pos: Pos(x as i64, y as i64),
+                    direction,
+                };
+
+                blizzards.push(blizzard);
+            }
+        }
+
+        let start = Pos(0, 1);
+        let goal = Pos(max_x, max_y - 1);
+        santa_possible_points.insert(start);
+
+        Ok(Self {
+            blizzards,
+            santa_possible_points,
+            max_x,
+            max_y,
+            goal,
+            minute: 0,
+        })
+    }
+}
+
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 use crate::solution::Solution;
 inventory::submit!(Solution::new(24, 1, p1));
